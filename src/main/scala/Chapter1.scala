945cd472d599d1b5d663f0509ab073caceb16e37
togashi-t <saladwithsalad@yahoo.co.jp>
2024-03-21 22:30:08 +0900

    1-4-2

--- src/main/scala/Chapter1.scala
+++ src/main/scala/Chapter1.scala
@@ -111,8 +111,9 @@ object Chapter1 extends App {
   }
 
 
-  // 1-4。文字列を並び替えることによって回文作成が可能かを返す。
-  def isRearrangeableToPalindrome(str: String): Boolean = {
+  // 文字列を並び替えることによって回文作成が可能かを返す。
+  // 1-4-1。数を数える方法。
+  def isRearrangeableToPalindromeA(str: String): Boolean = {
     val charCountMap = str
       .replaceAll(" ", "") // スペースは考慮の対象外なので除外
       .toLowerCase // 大文字小文字の区別はしないので全て小文字で処理する
@@ -121,8 +122,23 @@ object Chapter1 extends App {
     charCountMap.values.count(_ % 2 == 1) <= 1
   }
 
+  // 1-4-2。文字種類毎にtoggleする方法
+  def isRearrangeableToPalindromeB(str: String): Boolean = {
+    val oddNumberCharSet = scala.collection.mutable.Set.empty[Char]
+    val lowerCaseStr = str
+      .replaceAll(" ", "") // スペースは考慮の対象外なので除外
+      .toLowerCase // 大文字小文字の区別はしないので全て小文字で処理する
+    // 文字の出現ごとにSetの中の当該文字をtoggleする
+    for (char <- lowerCaseStr) {
+      if (oddNumberCharSet.contains(char)) oddNumberCharSet -= char else oddNumberCharSet += char
+    }
 
-  println(isRearrangeableToPalindrome("Aa Bb Cc d e "))
+    // 使用回数が奇数回の文字が1個以下の場合は回文作成が可能
+    oddNumberCharSet.size <= 1
+  }
+
+
+  println(isRearrangeableToPalindromeB("Aa Bb Cc d "))
 
 
 
