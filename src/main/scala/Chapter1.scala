fede7b3044baf49a4ce95ddbc2ac63318f04b458
togashi-t <saladwithsalad@yahoo.co.jp>
2024-03-24 17:19:32 +0900

    1-6

--- src/main/scala/Chapter1.scala
+++ src/main/scala/Chapter1.scala
@@ -1,4 +1,5 @@
 import scala.annotation.tailrec
+import scala.util.chaining.scalaUtilChainingOps
 
 object Chapter1 extends App {
 
@@ -187,13 +188,49 @@ object Chapter1 extends App {
   }
 
 
+  // 1-6。文字列圧縮。元の文字列よりも短くならなかった場合は元の文字列を返す
+  def compressStr(str: String): String = {
+    val strLength = str.length
+    // 圧縮結果を格納しているコレクション。圧縮結果が元のstrより短い場合のみ圧縮結果に意味があるため、その長さを設定。
+    val compressedStrBuilder = new StringBuilder(strLength - 1)
+
+    val maxIndex = strLength - 1
+
+    @tailrec
+    def loop(currentIndex: Int, currentCompressedLength: Int, consecutiveChar: Char, consecutiveCount: Int): String = {
+      if (currentCompressedLength >= strLength) { // 元の文字列より短くない場合
+        str
+      } else if (currentIndex > maxIndex) { // 末尾の文字まで処理が完了した場合
+        compressedStrBuilder.append(s"${consecutiveChar}${consecutiveCount}") // 連続した数を記録
+        compressedStrBuilder.toString
+      } else {
+        val currentChar = str(currentIndex)
+        if (currentChar == consecutiveChar) { // 文字の連続が継続している場合
+          loop(currentIndex + 1, currentCompressedLength, consecutiveChar, consecutiveCount + 1)
+        } else { // 文字の連続が経たれた場合
+          compressedStrBuilder.append(s"${consecutiveChar}${consecutiveCount}") // 連続した数を記録
+          loop(currentIndex + 1, currentCompressedLength + (1 + countDigit(consecutiveCount)) /** 文字の長さ＋数の長さ */, currentChar, 1)
+        }
+      }
+    }
+
+    loop(1, 0, str(0), 1)
+  }
+
+
+  // 整数の桁数を求める。n.toString.lengthだとnの数が大きい場合にパフォーマンスが悪化するため。
+  private def countDigit(n: Int): Int = {
+    if (n == 0) 1
+    else (math.log10(n.toDouble).pipe(math.floor) + 1).toInt // toDoubleを用いているのは、log10の引数の型はDoubleとする必要があるため。
+  }
 
 
 
-  println(isConvertable("pale", "ple"))
-  println(isConvertable("pales", "pale"))
-  println(isConvertable("pale", "bale"))
-  println(isConvertable("pale", "bake"))
+
+
+
+
+  println(compressStr("aabccdeeaaaaaaaaaaaaaa"))
 
 
 
