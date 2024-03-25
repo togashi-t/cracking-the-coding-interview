6b6f1fcfe177b659bb465af24732abfd6dab4340
togashi-t <saladwithsalad@yahoo.co.jp>
2024-03-25 23:27:18 +0900

    1-7

--- src/main/scala/Chapter1.scala
+++ src/main/scala/Chapter1.scala
@@ -226,11 +226,87 @@ object Chapter1 extends App {
 
 
 
+  // 1-7。行列を右に90度回転
+  def rotateA(matrix: Array[Array[Int]]): Array[Array[Int]] = {
+    // 1辺の長さ
+    val length = matrix.length
+    // 辺の長さが1以下の場合は回転する余地がないので即終了
+    if (length <= 1) {
+      matrix
+    } else {
+      // 外側の辺(=層)から順に他の辺の点を置換していく
+      for (layerIndex <- 0 until length / 2) {
+        val firstIndex = layerIndex
+        val lastIndex = length - 1 - layerIndex
+
+        for (i <- firstIndex until lastIndex) {
+          // 現在の要素からレイヤーの最初の位置までの距離
+          val offset = i - firstIndex
+
+          // 上辺の要素の値をとっておく
+          val tmp = matrix(firstIndex)(i)
+
+          // 左辺から上辺への移動
+          matrix(firstIndex)(i) = matrix(lastIndex - offset)(firstIndex)
+
+          // 下辺から左辺への移動
+          matrix(lastIndex - offset)(firstIndex) = matrix(lastIndex)(lastIndex - offset)
+
+          // 右辺から下辺への移動
+          matrix(lastIndex)(lastIndex - offset) = matrix(i)(lastIndex)
+
+          // 上側(保存した値) -> 右側
+          matrix(i)(lastIndex) = tmp
+        }
+      }
+
+      matrix
+    }
+  }
+
+
+
+  def rotateB(matrix: Array[Array[Int]]): Array[Array[Int]] = {
+    // 1辺の長さ
+    val length = matrix.length
+    // 辺の長さが1以下の場合は回転する余地がないので即終了
+    if (length <= 1) {
+      matrix
+    } else {
+      // 行列の入替
+      for {
+        rowIndex <- 0 until length
+        colIndex <- rowIndex + 1 until length
+      } {
+        val tmp = matrix(rowIndex)(colIndex)
+        matrix(rowIndex)(colIndex) = matrix(colIndex)(rowIndex)
+        matrix(colIndex)(rowIndex) = tmp
+      }
+
+      // 列の入替
+      for {
+        rowIndex <- 0 until length
+        colIndex <- 0 until length / 2
+      } {
+        val tmp = matrix(rowIndex)(colIndex)
+        matrix(rowIndex)(colIndex) = matrix(rowIndex)(length - 1 - colIndex)
+        matrix(rowIndex)(length - 1 - colIndex) = tmp
+      }
+
+      matrix
+    }
+
+  }
 
 
 
 
-  println(compressStr("aabccdeeaaaaaaaaaaaaaa"))
+
+
+
+
+  val matrix = Array(Array(1, 2, 3), Array(4, 5, 6), Array(7, 8, 9))
+  rotateB(matrix).foreach(x => x.foreach(println))
 
 
 
