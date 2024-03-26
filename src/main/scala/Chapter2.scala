dda0320c4d68d8cb5e5cc6f3a5626730f8d4f44c
togashi-t <saladwithsalad@yahoo.co.jp>
2024-03-26 18:21:29 +0900

    2-1-2

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -1,9 +1,9 @@
 object Chapter2 extends App {
 
-
-  // 2-1。ソートされていない連結リストから重複する要素を排除
+  // ソートされていない連結リストから重複する要素を排除
+  // 2-1-1。再帰を使用する方法。
   // a :: (b :: (c :: Nil))のように、Nilに対して前方から重複していない要素を追加していく格好になる。
-  def removeDuplicated[T](list: List[T]): List[T] = {
+  def removeDuplicatedA[T](list: List[T]): List[T] = {
 
     def loop(currentList: List[T], seen: Set[T]): List[T] = {
       currentList match {
@@ -21,12 +21,23 @@ object Chapter2 extends App {
   }
 
 
+  // 2-1-2。foldLeftを使用する方法。
+  def removeDuplicatedB[T](list: List[T]): List[T] = {
+    val (outcomeList, _) = list.foldLeft((List.empty[T], Set.empty[T])) { case ((tmpList, tmpSeen), element) =>
+      if (tmpSeen.contains(element)) { // 重複する要素の場合
+        (tmpList, tmpSeen)
+      } else { // 重複しない要素の場合
+        (element :: tmpList, tmpSeen + element)
+      }
+    }
+
+    outcomeList.reverse // 当初の順序とは逆になっているので元の順序に戻す
+  }
 
 
 
 
-
-  println(removeDuplicated(List(1,2, 3, 4,  1, 4)))
+  println(removeDuplicatedB(List(1,2, 3, 4,  1, 4)))
 
 
 
