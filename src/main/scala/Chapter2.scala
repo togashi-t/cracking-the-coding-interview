9c4b892484329fb6f19b264bd63d36071bce0cc8
togashi-t <saladwithsalad@yahoo.co.jp>
2024-03-26 21:45:48 +0900

    2-2

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -35,10 +35,28 @@ object Chapter2 extends App {
   }
 
 
+  // 2-2。後ろからK番目の要素を返す。2つのポインタを用いるランナーテクニックを使用。
+  // 1つ目のポインタが先行、2つ目のポイントは1つ目とはKの間隔を空ける。それぞれ同時に1つずつ進む。
+  def getElementFromLast[T](list: List[T], k: Int): Option[T] = {
+    val firstIterator = list.iterator.drop(k - 1)
 
+    if (!firstIterator.hasNext) { // そもそもlistの長さがkよりも短い場合
+      None
+    } else {
+      // listの先頭からk番目にいる
+      firstIterator.next()
+      // listの先頭の1つ手前にいる
+      val secondIterator = list.iterator
+      // 1つ目のポインタが末尾に到達するまで、両ポインタを同じ分だけ進める
+      while (firstIterator.hasNext) {
+        firstIterator.next()
+        secondIterator.next()
+      }
 
-  println(removeDuplicatedB(List(1,2, 3, 4,  1, 4)))
+      secondIterator.nextOption()
+    }
+  }
 
-
+  
 
 }
