40f18d0e2ad02ec8fe13fa48c21fd69a3142b895
togashi-t <saladwithsalad@yahoo.co.jp>
2024-03-27 18:33:37 +0900

    2-6-2

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -107,8 +107,9 @@ object Chapter2 extends App {
   }
 
 
-  // 2-6。回文であるかの判定
-  def isPalindrome[T](list: List[T]): Boolean = {
+  // 回文であるかの判定
+  // 2-6-1。リストを反転させて、元のリストと比べる。
+  def isPalindromeA[T](list: List[T]): Boolean = {
     @tailrec
     def loop(remainNormalList: List[T], remainReverseList: List[T]): Boolean = {
       (remainNormalList, remainReverseList) match {
@@ -123,10 +124,29 @@ object Chapter2 extends App {
   }
 
 
-  println(isPalindrome("abcdedcba".toList))
+  // 2-6-2。ランナーテクニックを使用。処理概要は以下。
+  // ・先頭から、一方ではリストの参照範囲を1ずつ狭めていき、もう一方では2ずつ狭めていく。
+  // ・1ずつ狭める際に要素を専用のリストに格納していく。(A)
+  // ・2ずつ狭めたものがリストの末尾に到達。1ずつ狭めたものはリストの真ん中に到達。(B)
+  // ・(A)と(B)の残りが合致するか確認
+  def isPalindromeB[T](list: List[T]): Boolean = {
+    @tailrec
+    def loop(restSlowList: List[T], restFastList: List[T], prevReverseList: List[T]): Boolean = {
+      restFastList match {
+        case Nil => // リストの長さが偶数
+          restSlowList == prevReverseList
+        case _ :: Nil => // リストの長さが奇数
+          restSlowList.tail == prevReverseList
+        case _ :: _ :: updatedRestFastList => // 終端まではまだ遠くリストの長さ不明
+          loop(restSlowList.tail, updatedRestFastList, restSlowList.head :: prevReverseList)
+      }
+    }
+
+    loop(list, list, List.empty[T])
+  }
 
 
-
+  println(isPalindromeB("abcdedcba".toList))
 
 
 
