4ac31a38e680ef5b9b8793b4383a1909d07a9fb5
togashi-t <saladwithsalad@yahoo.co.jp>
2024-04-02 23:54:45 +0900

    3-4refactoring

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -188,21 +188,23 @@ object Chapter3 extends App {
 
   // 3-4。スタックでキュー。2つのスタックを用いたキューを実装する。
   // pop用とpush用のスタックを用意する。
-  // push先はpopStackが空の場合のみpopStack、それ以外はpushStack。pushStackの一番上を最も古い要素とするため。
+  // push先は両Stackが空の場合のみpopStack、それ以外はpushStack。pushStackの一番上を最も古い要素とするため。
+  // 上記のようにするのは、pushStackを反転されるコストを少しでも減らしたいため。
   case class MyQueue[T](popStack: List[T] = Nil, pushStack: List[T] = Nil) {
 
-    def pop: (Option[T], MyQueue[T]) = {
-      popStack match {
-        case Nil => // 後述のとおりpopStackが空になったらpushStackから補充するので、このケースではpushStackもNilである。
-          (None, this)
-        case head :: Nil => // popによって空になったpopStackにpushStackから移す。pop効率のため順序を逆にして。
-          (Some(head), MyQueue(pushStack.reverse, Nil))
-        case head :: tail => // popStackから要素を取得するだけ
-          (Some(head), MyQueue(tail, pushStack))
+    @tailrec
+    final def pop: (Option[T], MyQueue[T]) = {
+      this match {
+        case MyQueue(Nil, Nil) =>
+          (None, MyQueue())
+        case MyQueue(Nil, push) =>
+          MyQueue(push.reverse, Nil).pop
+        case MyQueue(popHead :: popTail, _) =>
+          (Some(popHead), this.copy(popStack = popTail))
       }
     }
 
-    def push(value: T): MyQueue[T] = if (popStack.isEmpty)
+    def push(value: T): MyQueue[T] = if (popStack.isEmpty && pushStack.isEmpty)
       MyQueue(List(value), pushStack)
     else {
       MyQueue(popStack, value :: pushStack)
