a2bca2b270a84d9fefc0477e66204adb43de8c94
togashi-t <saladwithsalad@yahoo.co.jp>
2024-04-04 18:46:43 +0900

    4-1

--- src/main/scala/chapter4/Question1.scala
+++ src/main/scala/chapter4/Question1.scala
@@ -1,7 +1,8 @@
-import scala.annotation.tailrec
-import scala.util.chaining.scalaUtilChainingOps
+package chapter4
 
-object Chapter4 extends App {
+import scala.annotation.tailrec
+
+object Question1 extends App {
 
   // 4-1
   // 有向グラフが与えられたとき、2つのノード間に経路があるかどうかを判定するアルゴリズムを設計する。
@@ -19,10 +20,11 @@ object Chapter4 extends App {
   // ・ネットワークブロードキャスト: 最も近いノードから順に情報を伝えることで、効率的に全ノードに情報を広めることができる。
   def isPathPresentBFS(graph: Map[Int, List[Int]], start: Int, end: Int): Boolean = {
 
-    /***
+    /** *
      * 探索して到達可能かを返す
+     *
      * @param toVisitQueue 訪問すべきノード。Queueとした理由は先頭から探索したい、および後方に新たな探索先追加したいため。
-     * @param visitedSet 訪問済のノード。何度も同じノードに訪問する
+     * @param visitedSet   訪問済のノード。何度も同じノードに訪問する
      * @return
      */
     @tailrec
@@ -79,14 +81,6 @@ object Chapter4 extends App {
   }
 
 
-
-
-
-
-
-
-
-
   val graph: Map[Int, List[Int]] = Map(
     1 -> List(2, 3),
     2 -> List(4),
@@ -96,32 +90,12 @@ object Chapter4 extends App {
     6 -> List(3) // このエッジにより、3から4への間接的な経路が存在する
   )
 
-  // テストケース1: 経路が存在する場合
-  println("Test 1: Should find a path from 1 to 6")
-  println(isPathPresentDFS(graph, 1, 6)) // trueを期待
+  assert(isPathPresentDFS(graph, 1, 6))
 
+  assert(!isPathPresentDFS(graph, 5, 1))
 
-  // テストケース2: 経路が存在しない場合
-  println("Test 2: Should not find a path from 5 to 1")
-  println(isPathPresentDFS(graph, 5, 1)) // falseを期待
+  assert(isPathPresentDFS(graph, 3, 3))
 
-  // テストケース3: 開始ノードと終了ノードが同じ場合
-  println("Test 3: Should find a path from 3 to 3 (the same node)")
-  println(isPathPresentDFS(graph, 3, 3)) // trueを期待
-
-  // テストケース4: 間接的な経路が存在する場合
-  println("Test 4: Should find a path from 1 to 5 through an indirect route")
-  println(isPathPresentDFS(graph, 1, 5)) // trueを期待
-
-
-
-
-
-
-
-
-
-
-
+  assert(isPathPresentDFS(graph, 1, 5))
 
 }
