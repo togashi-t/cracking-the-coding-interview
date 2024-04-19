b48abe2aee90da0e99d7bd7950bf9428f2bc90fe
togashi-t <saladwithsalad@yahoo.co.jp>
2024-04-19 15:38:23 +0900

    add code

--- src/main/scala/chapter6/Question9.scala
+++ src/main/scala/chapter6/Question9.scala
@@ -1,6 +1,8 @@
 package chapter6
 
-object Question9 {
+import scala.annotation.tailrec
+
+object Question9 extends App {
 
   // 100個のロッカー:廊下に扉のしまったロッカーが100個あります。
   // まず、100のロッカーのすべての扉を間に(1つ飛びで)扉を閉めていきます。
@@ -13,7 +15,7 @@ object Question9 {
   //扉の開け閉めが何を意味しているのかをよく考えることで、この問題に取り組んでいきます。
 
   //疑問その1:扉が開け閉めされるのはいつ?
-  //n番目の扉が開け閉めされるのはnの約数と対応しています。たとえば15番目の原なら、1、3、5、15回目に開け閉めされます。
+  //n番目の扉が開け閉めされるのはnの約数と対応しています。たとえば15番目のロッカーなら、1、3、5、15回目に開け閉めされます。
 
   //疑問その2:開けたままになっている扉は?
   //開けたままになっているのは、扉番号の約数が奇数個になっているときです。約数2つを開閉の1セットと考えればわかりますね?
@@ -27,5 +29,34 @@ object Question9 {
   //1、4、9、16、25、36、49、64、81、100と数えていけば10個あることがわかります。単純に1から順に2乗の数を作ってもわかりますね。
   //1*1,2*2,3*3,..., 10*10
   //というわけで、10個のロッカーが最終的に開いているということになります。
+
+
+
+  // 愚直にプログラムで導出しようとすると以下
+  def openOrClose(lockerCount: Int): Array[Boolean] = {
+    // 各ロッカーの開閉状況を管理する配列作成。配列のindexとロッカーの番号が合致していた方が楽なので+1している。
+    val arr = Array.fill(lockerCount + 1)(false)
+
+    // 全ての約数で順にロッカーの開閉を行う。
+    // 全ての約数での開閉が完了した後の配列を返す。
+    @tailrec
+    def openOrCloseImpl(divisor: Int, index: Int): Array[Boolean] = {
+      if (divisor > lockerCount) { // 全ての約数での処理が終わった場合
+        arr
+      } else if (index > lockerCount) { // 現在の約数での処理が終わった場合。約数をインクリメント。
+        val newDivisor = divisor + 1
+        openOrCloseImpl(newDivisor, newDivisor)
+      } else { // ロッカーの開閉を行い、次のロッカーへ。
+        arr(index) = !arr(index)
+        openOrCloseImpl(divisor, index + divisor)
+      }
+    }
+
+    openOrCloseImpl(1, 1)
+  }
+
+
+  openOrClose(100).zipWithIndex.collect { case (bool, index) if bool => index }.foreach(println)
+
 
 }
